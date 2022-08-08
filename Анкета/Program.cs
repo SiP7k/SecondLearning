6da7477f8b8598bbfacc4479ea3987b35b733df1
Савва Чальцев <cavvach@gmail.com>
2022-08-08 13:05:30 +0300

    Добавили анкету

---
+++ Анкета/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class MainClass
+{
+	public static void Main(string[] args)
+	{
+
+		string MyName = "Savva";
+		byte MyAge = 21;
+		bool HaveIApet = true;
+		double MyShoeSize = 42.5;
+
+		Console.WriteLine("My name is " + MyName);
+		Console.WriteLine("MyAge " + MyAge);
+		Console.WriteLine("Do I have a pet? " + HaveIApet);
+		Console.WriteLine("My shoe size is " + MyShoeSize);
+
+	}
+}
